cfda628fd61c8ae3092e3d982126c16a84b4706d
BILAL26CK <bilal26ck@gmail.com>
2025-11-25 21:55:43 +0100

    Add files via upload

--- inout.cpp
+++ inout.cpp
@@ -1,24 +1,24 @@
-#pragma once
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <stdexcept>
 #include <limits>
-#include "INOUT.h"
+#include "inout.h"
 
 using namespace std;
 
-tDatos lectura(){
+tDatos lectura() {
     string in_operador;
     double in_num1, in_num2;
-    cout<<"\n> ";
-    if (cin >> in_operador >> in_num1 >> in_num2){
+    cout << "\n> ";
+    if (cin >> in_operador >> in_num1 >> in_num2) {
         tDatos input;
         input.operador = in_operador;
         input.num1 = in_num1;
         input.num2 = in_num2;
         return input;
-    } else {
+    }
+    else {
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         throw runtime_error("Error de lectura\n\nVerifica su input - [operacion] [int1] [int2]");
@@ -26,24 +26,30 @@ tDatos lectura(){
 }
 
 void mostrarSolucion(double sol) {
-    cout<< endl<<" :"<< sol;   
+    cout << endl << " :" << sol;
 }
 
-void mostrarError(string error){
-    cout<<"Ha ocurrido un error\n\n";
-    cout<<error;
+void mostrarError(string error) {
+    cout << "Ha ocurrido un error\n\n";
+    cout << error;
 }
 
-void mensajeInicio(){
-    cout<<"------ Calculadora -----"<<endl;
-    cout<<"Escribir el comando, el numero 1 y el numero 2\n\n";
-    cout<<"O solo uno de siguientes comandos:\n\t[Help] para mostrar operadores\n\t[Exit] para apagar calculadora";
+void mensajeInicio() {
+    cout << "------ Calculadora -----" << endl;
+    cout << "Escribir el comando, el numero 1 y el numero 2\n\n";
+    cout << "O solo uno de siguientes comandos:\n\t[Help] para mostrar operadores\n\t[Exit] para apagar calculadora";
 }
 
-void help(){
-    cout<<"Operaciones disponibles:"<<endl;
+void help() {
+    cout << "Operaciones disponibles:" << endl;
+    cout << "Help: muestra este mensaje de ayuda" << endl;
+    cout << "Add: suma ambos numeros" << endl;
+    cout << "Sub : resta el primero menos el segundo" << endl;
+    cout << "Mul : multiplica ambos numeros" << endl;
+    cout << "Div : divide el primero por el segundo" << endl;
+    cout << "Exp : eleva el primero al segundo" << endl;
 }
 
-double getNum1(tDatos datos) {return datos.num1;}
-double getNum2(tDatos datos) {return datos.num2;}
-string getOperator(tDatos datos) {return datos.operador;}
+double getNum1(tDatos datos) { return datos.num1; }
+double getNum2(tDatos datos) { return datos.num2; }
+string getOperator(tDatos datos) { return datos.operador; }
