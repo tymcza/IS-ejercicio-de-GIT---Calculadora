7caa8b5fbba33e885e5476be8463ae12344fbfeb
BILAL26CK <bilal26ck@gmail.com>
2025-11-25 22:28:35 +0100

    Add files via upload

---
+++ inout.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdexcept>
+#include <limits>
+using namespace std;
+
+struct tDatos {
+    double num1;
+    double num2;
+    string operador;
+};
+
+tDatos lectura();
+void mostrarSolucion(double sol);
+void mostrarError(string error);
+void mensajeInicio();
+void help();
+double getNum1(tDatos datos);
+double getNum2(tDatos datos);
+string getOperator(tDatos datos);
